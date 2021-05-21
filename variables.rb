1ee93918f8184c209b3c6dae16f2bcce4f2de147
Henry Schmid <henry.schmid1@gmail.com>
2021-05-21 17:50:59 -0600

    Add variables to variables.rb

---
+++ variables.rb
@@ -0,0 +1,13 @@
+animal = 'slow loris'
+is_cuddly = true
+animal = 'tiger'
+
+num_8 = 8.0
+num_3 = 3.0
+
+quotient = num_3 / num_8
+
+favorite_films = ['Pineapple Express', 'Boondock Saints', 'Snatch']
+favorite_films.shift
+
+puts animal.length
