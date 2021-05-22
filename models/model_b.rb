3b4abb408ebff6d1e7f675cdbeced94094f71f52
Henry Schmid <henry.schmid1@gmail.com>
2021-05-21 18:11:26 -0600

    Create house model using hashes

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+house_model = {
+  address: "123 Realname St.",
+  house_style: "neo-eclectic",
+  number_of_bathrooms: 3.5,
+  garage__car_capacity: 2,
+  is_dope: true,
+  has_hot_tub: false,
+  residents: ["Henry", "Tristan", "Obie"],
+  cool_features: ["doors", "windows", "pond", "fountain"]
+}
