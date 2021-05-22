d055c5c6cacc171c2b1148e5b51cf43aafbf6215
Henry Schmid <henry.schmid1@gmail.com>
2021-05-21 18:01:30 -0600

    Create laptop model using hashes in model_a.rb

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+laptop_model = {
+  brand: "Apple",
+  name: "Macbook Pro",
+  size_in_inches: 13,
+  number_of_screens: 1,
+  is_charged: true,
+  is_broken: false,
+  features: ["trackpad", "touch bar", "keyboard"],
+  type_of_ports: ["headphone", "USB-C"]
+}
